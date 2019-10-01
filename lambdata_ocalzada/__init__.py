7ee09e9c75e3929a43e3d1c35e024da38809c775
ocalzada <ocalzada@users.noreply.github.com>
2019-10-01 15:45:06 -0700

    Update version

--- lambdata_ocalzada/__init__.py
+++ lambdata_ocalzada/__init__.py
@@ -3,7 +3,14 @@ lambdata - a collection of data science helper functions
 """
 import pandas as pd
 
-df = pd.DataFrame({'Owner City State Zip': ["Los Angeles, CA 90015"]})
+# dataframe must be in City, State, Zip format
 
-regex = r'(?P<City>[^,]+)\s*,\s*(?P<State>[^\s]+)\s+(?P<Zip>\S+)'
-df['Owner City State Zip'].str.extract(regex)
+df = pd.DataFrame({'Owner City State Zip': ["Fontana, CA 92336", "Los Angeles, CA 90006", "Salt Lake City, UT 84106", "Brookline, MA 02445"
+                                           ]})
+
+# this function splits an address into its constituent City, State, Zip parts & 
+# returns a dataframe with each component in a separate column
+
+def split_address(df):
+    regex = r'(?P<City>[^,]+)\s*,\s*(?P<State>[^\s]+)\s+(?P<Zip>\S+)'
+    return df['Owner City State Zip'].str.extract(regex)
