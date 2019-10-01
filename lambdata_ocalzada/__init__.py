162c91fc27b119671ab912d9a105b059cafc27d3
ocalzada <ocalzada@users.noreply.github.com>
2019-10-01 13:05:20 -0700

    Create new helper function

--- lambdata_ocalzada/__init__.py
+++ lambdata_ocalzada/__init__.py
@@ -1,10 +1,24 @@
 """
 lambdata - a collection of data science helper functions
 """
-import pandas
-import numpy as np 
+import pandas as pd
+import requests
+import json
 
-# Code
+df = pd.read_csv('path to file')
 
-ONES = pandas.DataFrame(np.ones(10))
-ZEROS = pandas.DataFrame(np.zeros(50))
+# extract zips
+df['zip'] = df['Full Address'].apply(lambda x: x[-5:])
+
+# function to get city and state
+def get_city_state(zip):
+    url = 'http://ziptasticapi.com/{}'.format(zip)
+    response = requests.get(url)
+    adress_info = json.loads(response.content)
+    return adress_info['state'], adress_info['city']
+
+# add state_city as tuple to your df
+df['state_city'] = df['zip'].apply(lambda x: get_city_state(x))
+
+# split tuple into city and state
+df[['state', 'city']] = df['state_city'].apply(pd.Series)
