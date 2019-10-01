1eee3b79feb015b204fb3938b08b08e2fb2d815a
ocalzada <ocalzada@users.noreply.github.com>
2019-09-30 19:43:32 -0700

    Add Setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open("README.md", "r") as fh:
     description = "a collection of data science helper functions",
     long_description = LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
-    url="oscarcalzada.org",
+    url="https://github.com/ocalzada/lambdata-ocalzada/tree/master/lambdata_ocalzada",
     packages=setuptools.find_packages(),
     python_requires=">=3.5",
     install_requires = REQUIRED,
