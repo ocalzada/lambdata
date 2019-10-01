590d222f52f04219f7665885d994c8da06a56f77
ocalzada <ocalzada@users.noreply.github.com>
2019-09-30 17:54:41 -0700

    Added some proof of concept utility functions

---
+++ lambdata_ocalzada/__init__.py
@@ -0,0 +1,10 @@
+"""
+lambdata - a collection of data science helper functions
+"""
+import pandas
+import numpy as np 
+
+# Code
+
+ONES = pandas.DataFrame(np.ones(10))
+ZEROS = pandas.DataFrame(np.zeros(50))
