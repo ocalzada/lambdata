56225fc624ebb8a0b1bed55def689021464d777a
ocalzada <ocalzada@users.noreply.github.com>
2019-10-02 16:43:39 -0600

    update init, utils, and setup files

--- lambdata_ocalzada/__init__.py
+++ lambdata_ocalzada/__init__.py
@@ -1,16 +1,3 @@
 """
 lambdata - a collection of data science helper functions
 """
-import pandas as pd
-
-# dataframe must be in City, State, Zip format
-
-df = pd.DataFrame({'Owner City State Zip': ["Fontana, CA 92336", "Los Angeles, CA 90006", "Salt Lake City, UT 84106", "Brookline, MA 02445"
-                                           ]})
-
-# this function splits an address into its constituent City, State, Zip parts & 
-# returns a dataframe with each component in a separate column
-
-def split_address(df):
-    regex = r'(?P<City>[^,]+)\s*,\s*(?P<State>[^\s]+)\s+(?P<Zip>\S+)'
-    return df['Owner City State Zip'].str.extract(regex)
--- lambdata_ocalzada/df_utils.py
+++ lambdata_ocalzada/df_utils.py
@@ -1,7 +1,21 @@
-"""
-Utility functions for working with DataFrames
-"""
+import pandas as pd
 
-import pandas 
+# dataframe must be in City, State, Zip format
 
-TEST_DF = pandas.DataFrame([1,2,3])
+df = pd.DataFrame({'Owner City State Zip': ["Fontana, CA 92336", "Los Angeles, CA 90006", "Salt Lake City, UT 84106", "Brookline, MA 02445"
+                                           ]})
+
+# this function splits an address into its constituent City, State, Zip code parts & 
+# returns a dataframe with each component in a separate column
+
+class Splitter:
+    """"
+    Splitter - a class to help split-up databases into its components.
+    """
+    def __init__(self, df):
+        self.df = df
+        self.cols = df.columns
+        
+    def split_address(df):
+        regex = r'(?P<City>[^,]+)\s*,\s*(?P<State>[^\s]+)\s+(?P<Zip>\S+)'
+        return df['Owner City State Zip'].str.extract(regex)
